0b044c44bf8ac95689b8b92724943ab396efa069
Facuuv <facv.velazco@gmail.com>
2023-09-23 19:09:26 -0300

    deploy on render

--- chat-app/src/socket.js
+++ chat-app/src/socket.js
@@ -1,7 +1,7 @@
 
 import { io } from "socket.io-client"
 
-//const s = io('wss://192.168.0.2:3005/', { forceNew: true, autoConnect: false })
-const s = io('https://bgv647hp-3005.brs.devtunnels.ms/', { forceNew: true, autoConnect: false })
+//const s = io('http://192.168.0.2:3005/', { forceNew: true, autoConnect: false })
+const s = io('https://socket-fi88.onrender.com/', { forceNew: true, autoConnect: false })
 
 export default s
